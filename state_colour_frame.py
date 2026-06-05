8b0c0ffa39cd333e0db169406c1a66493a9e0a01
Gurpreet Kaur <86013655+23102003@users.noreply.github.com>
2026-06-05 17:39:22 +0530

    Update state_colour_frame.py

--- state_colour_frame.py
+++ state_colour_frame.py
@@ -829,7 +829,7 @@ if not focus_df.empty:
         
         # Get stats for this cluster (calculated from focus_df only)
         
-       stats_df = cluster_stats.loc[cluster_stats['cluster'] == c_name]
+        stats_df = cluster_stats.loc[cluster_stats['cluster'] == c_name]
         if len(stats_df) > 0:
            stats = stats_df.iloc[0]   
        
